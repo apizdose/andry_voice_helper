3784299952686bf389245fd10eb5a072ff6f00ab
Duff <59646857+apizdose@users.noreply.github.com>
2025-07-10 10:33:52 +0300

    Update voice.py

--- voice.py
+++ voice.py
@@ -1,8 +1,9 @@
 import pyttsx3
 
+
 def play_voice_assistant_speech(text_to_speech):
     ttsEngine=pyttsx3.init()
     voices = ttsEngine.getProperty("voices")
-    ttsEngine.setProperty("voice", voices[2].id)
+    ttsEngine.setProperty("voice", 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_RU-RU_IRINA_11.0')
     ttsEngine.say(str(text_to_speech))
     ttsEngine.runAndWait()
