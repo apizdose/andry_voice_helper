7acd8a22f481b92ebae3803737afb4643d98a404
Duff <59646857+apizdose@users.noreply.github.com>
2025-07-10 10:11:41 +0300

    Update voice.py

--- voice.py
+++ voice.py
@@ -1,14 +1,8 @@
 import pyttsx3
 
-ttsEngine=pyttsx3.init()
-voices = ttsEngine.getProperty("voices")
-ttsEngine.setProperty("voice", voices[2].id)
-print(voices)
-
 def play_voice_assistant_speech(text_to_speech):
-    """
-    Проигрывание речи ответов голосового ассистента (без сохранения аудио)
-    :param text_to_speech: текст, который нужно преобразовать в речь
-    """
+    ttsEngine=pyttsx3.init()
+    voices = ttsEngine.getProperty("voices")
+    ttsEngine.setProperty("voice", voices[2].id)
     ttsEngine.say(str(text_to_speech))
     ttsEngine.runAndWait()
